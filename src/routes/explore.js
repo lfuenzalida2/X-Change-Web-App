382b8541a642f67cab0fd0a467d6d78a58876a73
lfuenzalida2 <lfuenzalida2@uc.cl>
2020-05-12 00:27:28 -0400

    hotfix: search sin log in y amount of views de objetos

--- src/routes/explore.js
+++ src/routes/explore.js
@@ -5,12 +5,20 @@ const router = new KoaRouter();
 
 router.get('explore.list', '/', async (ctx) => {
   const currentUser = await ctx.state.currentUser;
-  const objectsList = await ctx.orm.object.findAll({
-    where: { userId: { [Op.not]: currentUser.id } } });
-  await ctx.render('explore/explore_list_object', {
-    objectsList,
-    view: (object) => ctx.router.url('objects.view', { id: object.id }),
-  });
+  try {
+    const objectsList = await ctx.orm.object.findAll({
+      where: { userId: { [Op.not]: currentUser.id } } });
+    await ctx.render('explore/explore_list_object', {
+      objectsList,
+      view: (object) => ctx.router.url('objects.view', { id: object.id }),
+    });
+  } catch {
+    const objectsList = await ctx.orm.object.findAll();
+    await ctx.render('explore/explore_list_object', {
+      objectsList,
+      view: (object) => ctx.router.url('objects.view', { id: object.id }),
+    });
+  }
 });
 
 
