7b1782dd397b64c9f79b8f26d25899e4afa59149
Sebastián Torés <37314438+topkaj@users.noreply.github.com>
2020-05-24 15:49:14 -0400

    most viewed on index (#21)
    Co-authored-by: topkaj <topkaj@outlook.com>

--- src/assets/js/code/carousel.js
+++ src/assets/js/code/carousel.js
@@ -4,15 +4,18 @@ function showSlides(n) {
   let i;
   const slides = document.getElementsByClassName('mySlides');
   const dots = document.getElementsByClassName('dot');
+  const captions = document.getElementsByClassName('caption');
   if (n > slides.length) { slideIndex = 1; }
   if (n < 1) { slideIndex = slides.length; }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none';
+    captions[i].style.display = 'none';
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(' active', '');
   }
   slides[slideIndex - 1].style.display = 'block';
+  captions[slideIndex - 1].style.display = 'block';
   dots[slideIndex - 1].className += ' active';
 }
 
@@ -31,9 +34,9 @@ window.onload = function () {
     showSlides(slideIndex);
     document.getElementsByClassName('prev')[0].addEventListener('click', () => plusSlides(-1), false);
     document.getElementsByClassName('next')[0].addEventListener('click', () => plusSlides(1), false);
-    const dots = document.getElementsByClassName('dots');
+    const dots = document.getElementsByClassName('dot');
     for (let i = 0; i < dots.length; i++) {
-      dots[i].addEventListener('click', currentSlide(i + 1), false);
+      dots[i].addEventListener('click', () => currentSlide(i + 1), false);
     }
   }
 };
