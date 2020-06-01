74e7a974f12f9e031e116f5766526ebb43d873b0
lfuenzalida2 <lfuenzalida2@uc.cl>
2020-06-01 15:43:56 -0400

    images and icons uploaded in assets/images/

--- src/routes.js
+++ src/routes.js
@@ -51,15 +51,6 @@ router.use('/negotiations', async (ctx, next) => {
   await next(); // go to next middleware
 });
 
-// No permite a un usuario cualquiera entrar al la url /users, los devuelve
-router.use('/users', async (ctx, next) => {
-  if (!ctx.state.currentUser) {
-    ctx.redirect('/');
-    return;
-  }
-  await next(); // go to next middleware
-});
-
 router.use('/notifications', async (ctx, next) => {
   if (!ctx.state.currentUser) {
     ctx.redirect('/session/new');
