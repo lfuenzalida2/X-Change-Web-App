dacbc3e191d5da55c31e791e7898dabd2c850e91
lfuenzalida2 <lfuenzalida2@uc.cl>
2020-06-29 21:17:21 -0400

    Asegura las apis utilizadas de manera interna

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -4,6 +4,12 @@ const fileStorage = require('../../services/file-storage');
 
 const router = new KoaRouter();
 
+function Authentification(currentUser) {
+  if (!currentUser) {
+    throw Error('Porfavor utiliza un browser para utilizar esta función');
+  }
+}
+
 function sortByDateDesc(a, b) {
   return -(new Date(a.updatedAt).getTime() - new Date(b.updatedAt).getTime());
 }
@@ -14,159 +20,219 @@ function ExceptionName(mensaje) {
 }
 
 router.patch('api.upload', '/upload', async (ctx) => {
+  const { currentUser } = ctx.state;
   try {
+    Authentification(currentUser);
     if (!ctx.request) {
       throw new ExceptionName('No hay archivos para subir');
     }
-    const currentUser = await ctx.state.currentUser;
     const { file } = ctx.request.files;
     file.name = `${currentUser.id}_profile_${new Date().toLocaleString()}${file.name.slice(file.name.lastIndexOf('.'), file.name.length)}`;
     const profilePicture = file.name;
     await fileStorage.upload(file);
     await currentUser.update({ profilePicture });
     ctx.body = ctx.jsonSerializer('res', 'upload succesful');
-  } catch (validationError) {
-    ctx.body = validationError;
+  } catch (err) {
     ctx.status = 400;
+    ctx.body = { message: err.message };
   }
 });
 
 router.get('api.negotiation.get.negotiations', '/negotiations', async (ctx) => {
   const users = await ctx.orm.user;
-  const currentUser = await ctx.state.currentUser;
-  const negotiationsList = await ctx.orm.negotiation.findAll({
-    where: {
-      [Op.or]: [{ sellerId: { [Op.eq]: currentUser.id } },
-        { customerId: { [Op.eq]: currentUser.id } }],
-    },
-    include: [{ model: users, as: 'customer' }, { model: users, as: 'seller' }],
-  });
-  negotiationsList.sort(sortByDateDesc);
-  ctx.body = ctx.jsonSerializer('negotiation', {
-    attributes: ['state', 'customer', 'seller'],
-  }).serialize(negotiationsList);
+  const { currentUser } = ctx.state;
+  try {
+    Authentification(currentUser);
+    const negotiationsList = await ctx.orm.negotiation.findAll({
+      where: {
+        [Op.or]: [{ sellerId: { [Op.eq]: currentUser.id } },
+          { customerId: { [Op.eq]: currentUser.id } }],
+      },
+      include: [{ model: users, as: 'customer' }, { model: users, as: 'seller' }],
+    });
+    negotiationsList.sort(sortByDateDesc);
+    ctx.body = ctx.jsonSerializer('negotiation', {
+      attributes: ['state', 'customer', 'seller'],
+    }).serialize(negotiationsList);
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = { message: err.message };
+  }
 });
 
 router.get('api.current.user', '/current_user', async (ctx) => {
-  const currentUser = await ctx.state.currentUser;
-  ctx.body = ctx.jsonSerializer('currentUser', {
-    attributes: ['id', 'username', 'profilePicture'],
-  }).serialize(currentUser);
+  const { currentUser } = ctx.state;
+  try {
+    Authentification(currentUser);
+    ctx.body = ctx.jsonSerializer('currentUser', {
+      attributes: ['id', 'username', 'profilePicture'],
+    }).serialize(currentUser);
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = { message: err.message };
+  }
 });
 
 router.get('api.negotiation.review', '/review/:id/:rid/:red', async (ctx) => {
   const { id, rid, red } = ctx.params;
-  const review = await ctx.orm.review.findOne({
-    where: { negotiationId: id, reviewerId: rid, reviewedId: red },
-  });
-  ctx.body = ctx.jsonSerializer('review', {
-    attributes: ['rating', 'text'],
-  }).serialize(review);
+  const { currentUser } = ctx.state;
+  try {
+    Authentification(currentUser);
+    const review = await ctx.orm.review.findOne({
+      where: { negotiationId: id, reviewerId: rid, reviewedId: red },
+    });
+    ctx.body = ctx.jsonSerializer('review', {
+      attributes: ['rating', 'text'],
+    }).serialize(review);
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = { message: err.message };
+  }
 });
 
 router.get('api.negotiation.messagges', '/messagges/:id', async (ctx) => {
-  const negotiation = await ctx.orm.negotiation.findByPk(ctx.params.id);
-  const messaggesList = await negotiation.getMessages();
-  ctx.body = ctx.jsonSerializer('messagge', {
-    attributes: ['senderId', 'receiverId', 'text', 'createdAt'],
-  }).serialize(messaggesList);
+  const { currentUser } = ctx.state;
+  try {
+    Authentification(currentUser);
+    const negotiation = await ctx.orm.negotiation.findByPk(ctx.params.id);
+    const messaggesList = await negotiation.getMessages();
+    ctx.body = ctx.jsonSerializer('messagge', {
+      attributes: ['senderId', 'receiverId', 'text', 'createdAt'],
+    }).serialize(messaggesList);
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = { message: err.message };
+  }
 });
 
 router.get('api.categories', '/categories', async (ctx) => {
-  const categories = await ctx.orm.category.findAll();
-  ctx.body = ctx.jsonSerializer('categories', {
-    attributes: ['id', 'name'],
-  }).serialize(categories);
+  const { currentUser } = ctx.state;
+  try {
+    Authentification(currentUser);
+    const categories = await ctx.orm.category.findAll();
+    ctx.body = ctx.jsonSerializer('categories', {
+      attributes: ['id', 'name'],
+    }).serialize(categories);
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = { message: err.message };
+  }
 });
 
 router.post('api.objects.create', '/object_create', async (ctx) => {
-  const object = ctx.orm.object.build(ctx.request.body);
-  const user = ctx.state.currentUser;
+  const { currentUser } = ctx.state;
   try {
+    Authentification(currentUser);
+    const object = ctx.orm.object.build(ctx.request.body);
+    const user = ctx.state.currentUser;
     object.userId = user.id;
     await object.save({ fields: ['views', 'userId', 'categoryId', 'name', 'state', 'description'] });
     ctx.body = { id: user.id };
-  } catch (validationError) {
-    ctx.body = validationError;
+  } catch (err) {
     ctx.status = 400;
+    ctx.body = { message: err.message };
   }
 });
 
 router.get('api.objects.list', '/:id', async (ctx) => {
   const { currentUser } = ctx.state;
-  const categories = ctx.orm.category;
-  const negotiations = ctx.orm.negotiation;
-  const photos = ctx.orm.photo;
-  const objectsList = await ctx.orm.object.findAll({
-    where: { userId: currentUser.id },
-    include: [{ model: categories }, { model: negotiations }, { model: photos }],
-  });
-  ctx.body = ctx.jsonSerializer('object', {
-    attributes: ['name', 'description', 'state', 'category', 'negotiations', 'photos'],
-  }).serialize(objectsList);
+  try {
+    Authentification(currentUser);
+    const categories = ctx.orm.category;
+    const negotiations = ctx.orm.negotiation;
+    const photos = ctx.orm.photo;
+    const objectsList = await ctx.orm.object.findAll({
+      where: { userId: currentUser.id },
+      include: [{ model: categories }, { model: negotiations }, { model: photos }],
+    });
+    ctx.body = ctx.jsonSerializer('object', {
+      attributes: ['name', 'description', 'state', 'category', 'negotiations', 'photos'],
+    }).serialize(objectsList);
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = { message: err.message };
+  }
 });
 
 router.get('api.objects.list_other', '/other/:id', async (ctx) => {
   const { currentUser } = ctx.state;
-  const negotiation = await ctx.orm.negotiation.findByPk(ctx.params.id);
-  const categories = ctx.orm.category;
-  const negotiations = ctx.orm.negotiation;
-  const photos = ctx.orm.photo;
-  if (negotiation.sellerId === currentUser.id) {
-    const objectsList = await ctx.orm.object.findAll({
-      where: { userId: negotiation.customerId },
-      include: [{ model: categories }, { model: negotiations }, { model: photos }],
-    });
-    ctx.body = ctx.jsonSerializer('object', {
-      attributes: ['name', 'description', 'state', 'category', 'negotiations', 'photos'],
-    }).serialize(objectsList);
-  } else if (negotiation.customerId === currentUser.id) {
-    const objectsList = await ctx.orm.object.findAll({
-      where: { userId: negotiation.sellerId },
-      include: [{ model: categories }, { model: negotiations }, { model: photos }],
-    });
-    ctx.body = ctx.jsonSerializer('object', {
-      attributes: ['name', 'description', 'state', 'category', 'negotiations', 'photos'],
-    }).serialize(objectsList);
+  try {
+    Authentification(currentUser);
+    const negotiation = await ctx.orm.negotiation.findByPk(ctx.params.id);
+    const categories = ctx.orm.category;
+    const negotiations = ctx.orm.negotiation;
+    const photos = ctx.orm.photo;
+    if (negotiation.sellerId === currentUser.id) {
+      const objectsList = await ctx.orm.object.findAll({
+        where: { userId: negotiation.customerId },
+        include: [{ model: categories }, { model: negotiations }, { model: photos }],
+      });
+      ctx.body = ctx.jsonSerializer('object', {
+        attributes: ['name', 'description', 'state', 'category', 'negotiations', 'photos'],
+      }).serialize(objectsList);
+    } else if (negotiation.customerId === currentUser.id) {
+      const objectsList = await ctx.orm.object.findAll({
+        where: { userId: negotiation.sellerId },
+        include: [{ model: categories }, { model: negotiations }, { model: photos }],
+      });
+      ctx.body = ctx.jsonSerializer('object', {
+        attributes: ['name', 'description', 'state', 'category', 'negotiations', 'photos'],
+      }).serialize(objectsList);
+    }
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = { message: err.message };
   }
 });
 
 router.get('api.negotiation.get', '/negotiation/:id', async (ctx) => {
   const { currentUser } = ctx.state;
-  const negotiation = await ctx.orm.negotiation.findByPk(ctx.params.id);
-  let other;
-  if (negotiation.customerId === currentUser.id) {
-    other = await negotiation.getSeller();
-  } else {
-    other = await negotiation.getCustomer();
+  try {
+    Authentification(currentUser);
+    const negotiation = await ctx.orm.negotiation.findByPk(ctx.params.id);
+    let other;
+    if (negotiation.customerId === currentUser.id) {
+      other = await negotiation.getSeller();
+    } else {
+      other = await negotiation.getCustomer();
+    }
+    ctx.body = ctx.jsonSerializer('negotiation', {
+      attributes: ['customerId', 'sellerId', 'state'],
+    }).serialize(negotiation);
+    ctx.body.other = other.username;
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = { message: err.message };
   }
-  ctx.body = ctx.jsonSerializer('negotiation', {
-    attributes: ['customerId', 'sellerId', 'state'],
-  }).serialize(negotiation);
-  ctx.body.other = other.username;
 });
 
 router.del('api.object_del', '/:id/object', async (ctx) => {
-  ctx.state.negotiation = await ctx.orm.negotiation.findByPk(ctx.params.id);
-  const objectNegotiation = ctx.orm.objectNegotiation.build(ctx.request.body);
-  await objectNegotiation.destroy();
-  ctx.redirect(ctx.router.url('api.objects.list', { id: ctx.params.id }));
+  const { currentUser } = ctx.state;
+  try {
+    Authentification(currentUser);
+    ctx.state.negotiation = await ctx.orm.negotiation.findByPk(ctx.params.id);
+    const objectNegotiation = ctx.orm.objectNegotiation.build(ctx.request.body);
+    await objectNegotiation.destroy();
+    ctx.redirect(ctx.router.url('api.objects.list', { id: ctx.params.id }));
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = { message: err.message };
+  }
 });
 
 router.post('api.object_add', '/:id/object', async (ctx) => {
-  const negotiation = await ctx.orm.negotiation.findByPk(ctx.params.id);
-  const objectNegotiation = ctx.orm.objectNegotiation.build(ctx.request.body);
+  const { currentUser } = ctx.state;
   try {
+    Authentification(currentUser);
+    const negotiation = await ctx.orm.negotiation.findByPk(ctx.params.id);
+    const objectNegotiation = ctx.orm.objectNegotiation.build(ctx.request.body);
     await objectNegotiation.save({ fields: ['negotiationId', 'objectId'] });
     negotiation.changed('updatedAt', true);
     await negotiation.save();
-    const respuesta = { status: 200, text: 'gucci la wea salió fina' };
     ctx.redirect(ctx.router.url('api.objects.list', { id: ctx.params.id }));
   } catch (err) {
-    const respuesta = { status: 400, text: 'miegda la wea se ha caido' };
-    ctx.body = ctx.jsonSerializer('respuesta', {
-      attributes: ['status', 'text'],
-    }).serialize(respuesta);
+    ctx.status = 400;
+    ctx.body = { message: err.message };
   }
 });
 
